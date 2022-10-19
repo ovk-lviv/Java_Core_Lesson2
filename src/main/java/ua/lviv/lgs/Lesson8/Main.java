082729f82c9f8b8a77aec16c0b8c431df9780b95
Olena Kovalska <Olena.Kovalska@atrg.com>
2022-10-19 16:58:17 +0300

    lesson9 commit #1

--- src/main/java/ua/lviv/lgs/Lesson8/Main.java
+++ src/main/java/ua/lviv/lgs/Lesson8/Main.java
@@ -1,5 +1,6 @@
 package ua.lviv.lgs.Lesson8;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import static ua.lviv.lgs.Lesson8.Service.menu;
@@ -12,6 +13,7 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
+
         String month; // variable is used multiple times
 
 // values fetched as they are used multiple times
@@ -23,167 +25,172 @@ public class Main {
         int numberOfDays = 0;
         int indexSeason;
         boolean flag;
+        try {
 
-        switch (number) {
+            switch (number) {
 
-            case 1:
-                System.out.println("Enter month...");
-                month = scanner.next().toUpperCase();
-                flag = false;
-                for (Service.Months m : arrMonths) {
-                    if (m.name().equals(month)) {
-                        flag = true;
-                        System.out.println("Such month exists");
-                    }
-                }
-                if (!flag) {
-                    System.out.println("No such month");
-                }
-                break;
-
-            case 2:
-                System.out.println("Enter month...");
-                month = scanner.next().toUpperCase();
-                for (Service.Months m : arrMonths) {
-                    if (m.name().equals(month)) {
-                        seasonName = m.getSeason().toString();
-                    }
-                }
-                for (Service.Months m : arrMonths) {
-                    if (m.getSeason().toString().equals(seasonName)) {
-                        System.out.println(m.name());
-                    }
-                }
-                break;
-
-            case 3:
-                System.out.println("Enter month...");
-                month = scanner.next().toUpperCase();
-                for (Service.Months m : arrMonths) {
-                    if (m.name().equals(month)) {
-                        numberOfDays = m.getDays();
-                    }
-                }
-                for (Service.Months m : arrMonths) {
-                    if (m.getDays() == numberOfDays) {
-                        System.out.println(m.name());
-                    }
-                }
-                break;
-
-            case 4:
-                System.out.println("Enter month...");
-                month = scanner.next().toUpperCase();
-                flag = false;
-                for (Service.Months m : arrMonths) {
-                    if (m.name().equals(month)) {
-                        numberOfDays = m.getDays();
-                    }
-                }
-                for (Service.Months m : arrMonths) {
-                    if (m.getDays() < numberOfDays) {
-                        System.out.println(m.name());
-                        flag = true;
-                    }
-
-                }
-                if (!flag) {
-                    System.out.println("There are no months with fewer number of days");
-                }
-
-                break;
-
-            case 5:
-                System.out.println("Enter month...");
-                month = scanner.next().toUpperCase();
-                flag = false;
-                for (Service.Months m : arrMonths) {
-                    if (m.name().equals(month)) {
-                        numberOfDays = m.getDays();
-                    }
-                }
-                for (Service.Months m : arrMonths) {
-                    if (m.getDays() > numberOfDays) {
-                        System.out.println(m.name());
-                        flag = true;
-                    }
-
-                }
-                if (!flag) {
-                    System.out.println("There are no months with larger number of days");
-                }
-                break;
-            // the next season is calculated considering season's index in enum
-            case 6:
-                System.out.println("Enter month...");
-                month = scanner.next().toUpperCase();
-                for (Service.Months m : arrMonths) {
-                    if (m.name().equals(month)) {
-                        indexSeason = m.season.ordinal();
-                        //for cases when new index can go outside array
-                        if (indexSeason >= arrSeasons.length - 1) {
-                            System.out.println(arrSeasons[0]);
-                        } else {
-                            System.out.println(arrSeasons[indexSeason + 1]);
+                case 1:
+                    System.out.println("Enter month...");
+                    month = scanner.next().toUpperCase();
+                    flag = false;
+                    for (Service.Months m : arrMonths) {
+                        if (m.name().equals(month)) {
+                            flag = true;
+                            System.out.println("Such month exists");
                         }
                     }
-                }
-                break;
-            // the previous season is calculated considering season's index in enum
-            case 7:
-                System.out.println("Enter month...");
-                month = scanner.next().toUpperCase();
-                for (Service.Months m : arrMonths) {
-                    if (m.name().equals(month)) {
-                        indexSeason = m.season.ordinal();
-                        // for cases when new index can go outside array
-                        if (indexSeason == 0) {
-                            System.out.println(arrSeasons[arrSeasons.length - 1]);
-                        } else {
-                            System.out.println(arrSeasons[indexSeason - 1]);
+                    if (!flag) {
+                        System.out.println("No such month");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Enter month...");
+                    month = scanner.next().toUpperCase();
+                    for (Service.Months m : arrMonths) {
+                        if (m.name().equals(month)) {
+                            seasonName = m.getSeason().toString();
                         }
                     }
-                }
-                break;
-
-            case 8:
-                for (Service.Months m : arrMonths) {
-                    if (m.getDays() % 2 == 0) {
-                        System.out.println(m.name());
+                    for (Service.Months m : arrMonths) {
+                        if (m.getSeason().toString().equals(seasonName)) {
+                            System.out.println(m.name());
+                        }
                     }
-                }
-                break;
+                    break;
 
-            case 9:
-                for (Service.Months m : arrMonths) {
-                    if (m.getDays() % 2 != 0) {
-                        System.out.println(m.name());
+                case 3:
+                    System.out.println("Enter month...");
+                    month = scanner.next().toUpperCase();
+                    for (Service.Months m : arrMonths) {
+                        if (m.name().equals(month)) {
+                            numberOfDays = m.getDays();
+                        }
                     }
-                }
-                break;
+                    for (Service.Months m : arrMonths) {
+                        if (m.getDays() == numberOfDays) {
+                            System.out.println(m.name());
+                        }
+                    }
+                    break;
 
-            case 0:
-                System.out.println("Enter month...");
-                month = scanner.next().toUpperCase();
-                flag = false;
-                for (Service.Months m : arrMonths) {
-                    if (m.name().equals(month)) {
-                        if (m.getDays() % 2 == 0) {
-                            System.out.println("Month has even number of days");
+                case 4:
+                    System.out.println("Enter month...");
+                    month = scanner.next().toUpperCase();
+                    flag = false;
+                    for (Service.Months m : arrMonths) {
+                        if (m.name().equals(month)) {
+                            numberOfDays = m.getDays();
+                        }
+                    }
+                    for (Service.Months m : arrMonths) {
+                        if (m.getDays() < numberOfDays) {
+                            System.out.println(m.name());
                             flag = true;
                         }
-                    }
-                }
-                if (!flag) {
-                    System.out.println("Month does not have even number of days");
-                }
-                break;
-            default:
-                System.out.println("Something went wrong! Check your inputs");
 
+                    }
+                    if (!flag) {
+                        System.out.println("There are no months with fewer number of days");
+                    }
+
+                    break;
+
+                case 5:
+                    System.out.println("Enter month...");
+                    month = scanner.next().toUpperCase();
+                    flag = false;
+                    for (Service.Months m : arrMonths) {
+                        if (m.name().equals(month)) {
+                            numberOfDays = m.getDays();
+                        }
+                    }
+                    for (Service.Months m : arrMonths) {
+                        if (m.getDays() > numberOfDays) {
+                            System.out.println(m.name());
+                            flag = true;
+                        }
+
+                    }
+                    if (!flag) {
+                        System.out.println("There are no months with larger number of days");
+                    }
+                    break;
+                // the next season is calculated considering season's index in enum
+                case 6:
+                    System.out.println("Enter month...");
+                    month = scanner.next().toUpperCase();
+                    for (Service.Months m : arrMonths) {
+                        if (m.name().equals(month)) {
+                            indexSeason = m.season.ordinal();
+                            //for cases when new index can go outside array
+                            if (indexSeason >= arrSeasons.length - 1) {
+                                System.out.println(arrSeasons[0]);
+                            } else {
+                                System.out.println(arrSeasons[indexSeason + 1]);
+                            }
+                        }
+                    }
+                    break;
+                // the previous season is calculated considering season's index in enum
+                case 7:
+                    System.out.println("Enter month...");
+                    month = scanner.next().toUpperCase();
+                    for (Service.Months m : arrMonths) {
+                        if (m.name().equals(month)) {
+                            indexSeason = m.season.ordinal();
+                            // for cases when new index can go outside array
+                            if (indexSeason == 0) {
+                                System.out.println(arrSeasons[arrSeasons.length - 1]);
+                            } else {
+                                System.out.println(arrSeasons[indexSeason - 1]);
+                            }
+                        }
+                    }
+                    break;
+
+                case 8:
+                    for (Service.Months m : arrMonths) {
+                        if (m.getDays() % 2 == 0) {
+                            System.out.println(m.name());
+                        }
+                    }
+                    break;
+
+                case 9:
+                    for (Service.Months m : arrMonths) {
+                        if (m.getDays() % 2 != 0) {
+                            System.out.println(m.name());
+                        }
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Enter month...");
+                    month = scanner.next().toUpperCase();
+                    flag = false;
+                    for (Service.Months m : arrMonths) {
+                        if (m.name().equals(month)) {
+                            if (m.getDays() % 2 == 0) {
+                                System.out.println("Month has even number of days");
+                                flag = true;
+                            }
+                        }
+                    }
+                    if (!flag) {
+                        System.out.println("Month does not have even number of days");
+                    }
+                    break;
+                default:
+                    System.out.println("Something went wrong!");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            }
         }
     }
-}
+
+
 
 
 
---
+++ src/main/java/ua/lviv/lgs/Lesson8/WrongInputConsoleParametersException.java
@@ -0,0 +1,8 @@
+package ua.lviv.lgs.Lesson8;
+
+public class WrongInputConsoleParametersException extends Exception {
+     public WrongInputConsoleParametersException(String message) {
+         super(message);
+     }
+
+}
